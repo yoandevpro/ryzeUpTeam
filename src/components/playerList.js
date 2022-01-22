2c39b1c15c313b30469e79ea630ca3096e804af6
patronus <yoandevpro@gmail.com>
2022-01-22 20:15:07 +0100

    fix problem merge

--- src/components/playerList.js
+++ src/components/playerList.js
@@ -1,23 +1,3 @@
-<<<<<<< HEAD
-import PlayerCard from './playerCard'
-import {team,ENVyoan, ENVvincent , ENVthibault , ENVoscar , ENVpaul , ENValex} from '../constantes/ENV.js'
-console.log(team)
-    // const cardTeam = [yoanCard,vincentCard,thibaultCard,oscarCard,pauloCard]
-function PlayerList(){
-    return(
-        // team.map((player, key) => <div className='playerList'><PlayerCard key={key} username={team[player]}/></div>)
-        <div className='playerList'>    
-            <PlayerCard username={ENVyoan}/>
-            <PlayerCard username={ENVvincent}/>
-            <PlayerCard username={ENVthibault}/>
-            <PlayerCard username={ENVoscar}/>
-            <PlayerCard username={ENVpaul}/>
-            <PlayerCard username={ENValex}/>
-        </div>
-    )
-}
-export default PlayerList
-=======
 import PlayerCard from "./playerCard";
 
 // const cardTeam = [yoanCard,vincentCard,thibaultCard,oscarCard,pauloCard]
@@ -33,4 +13,3 @@ function PlayerList(props) {
   );
 }
 export default PlayerList;
->>>>>>> bdce5339d34f31e95af0a2b5ae5b08acd4ea024b
